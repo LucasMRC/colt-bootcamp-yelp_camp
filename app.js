8ad04ae87d35d48dd54fd7b2b00aef97f9678f37
Lucas Reyna Cordoba <lucas.reynacordoba22@gmail.com>
2019-01-17 21:21:37 +0000

    enviroment variable set

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var bodyParser      = require('body-parser'),
     app             = express();
 
 // APP CONFIG
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
 
-mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+mongoose.connect(url, {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
